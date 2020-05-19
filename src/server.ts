1e79738b1eda31375e0cd259d44a3df49e2a7676
diego-git-1 <diegocesar.dev@gmail.com>
2020-05-19 15:10:57 -0300

    end initial setup

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import express from 'express'
+import routes from './routes'
+
+const app = express()
+const PORT = 3333
+
+app.use(routes)
+
+app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}/`))
